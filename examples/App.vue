0ff72c0247c850ea0e91ee041d4c9547e750e86b
yellowsea <yunhai0644@163.com>
2022-06-08 14:41:12 +0800

    chore: add button componment

---
+++ examples/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <M-Button type="main" size="small" icon="correct">封装成功</M-Button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+
+</style>
--- examples/main.js
+++ examples/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import "../UI/packages/css/button.scss"
+import Button from '../UI/packages/button/index'
+Vue.use(Button)
 Vue.config.productionTip = false
 
 new Vue({
