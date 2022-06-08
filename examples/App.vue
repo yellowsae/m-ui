c01582caa84e940ed0b06ea242b11ca6dfd2f80b
yellowsea <yunhai0644@163.com>
2022-06-08 14:44:23 +0800

    chore: add Badge componment

--- examples/App.vue
+++ examples/App.vue
@@ -1,8 +1,24 @@
 <template>
   <div id="app">
-    <M-Button type="main" size="small" icon="correct">封装成功</M-Button>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <M-Button  type="main"  size="small" icon="correct">封装成功</M-Button>
+    <div style="width:500px; display:flex;justify-content: space-between;">
+      <M-Badge :value="1" type="success">
+        <M-Button size="small">Number类型</M-Button>
+      </M-Badge>
+      <M-Badge :value="100" :max="99" type="warning">
+        <M-Button size="small">限制最大值</M-Button>
+      </M-Badge>
+      <M-Badge is-dot>
+        <M-Button size="small" icon="look">小圆点</M-Button>
+      </M-Badge>
+      <M-Badge value="hot" type="primary">
+        <M-Button size="small" >String类型</M-Button>
+      </M-Badge>   
+    </div>
   </div>
 </template>
+
 
 <script>
 
--- examples/main.js
+++ examples/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 import "../UI/packages/css/button.scss"
 import Button from '../UI/packages/button/index'
+import Badge from '../UI/packages/badge/index'
+import "../UI/packages/css/badge.scss"
+
+
+Vue.use(Badge)
 Vue.use(Button)
 Vue.config.productionTip = false
 
